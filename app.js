615a65b189566506a30529cf9262e57858120ae8
Greimil <greimilnunez@gmail.com>
2021-08-04 10:10:50 -0400

    pattern chanche to classes

--- app.js
+++ app.js
@@ -14,13 +14,17 @@ const $pantalla = document.querySelector(".pantalla")
 
 let contador = 0
 
-//Constructor de libros
-function Book(title, author, pages,read) {
-    this.title = title
-    this.author = author
-    this.pages = pages
-    this.read = read
+
+class Book{
+    constructor(title, author, pages,read){
+        this.title = title
+        this.author = author
+        this.pages = pages
+        this.read = read
+    }
 }
+
+
 
 
 // funcion agregar libros
